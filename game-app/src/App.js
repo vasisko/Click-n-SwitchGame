c20eab202083081a79a72a11cf9581353c1a41a5
Carolyn Vasisko <carolyn@mothermedia.com>
2018-06-11 23:00:55 -0500

    initial files with cards displaying on page

---
+++ game-app/src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import GameCard from "./components/GameCard";
+import Wrapper from "./components/Wrapper";
+import gamecards from "./gamecards.json";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    gamecards
+  };
+
+  removeCard = id => {
+    console.log(id);
+    const gamecards = this.state.gamecards.filter(gamecard => gamecard.id !== id);
+    this.setState({ gamecards });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+      <h1 className="title">Click a Card!</h1>
+       {gamecards.map(gamecard =>
+      <GameCard 
+        removeFriend={this.removeCard}
+        id={gamecard.id}
+        key={gamecard.id}
+        name={gamecard.name}
+        image={gamecard.image}
+      />)};
+    </Wrapper>)}
+    
+};
+
+export default App;
---
+++ game-app/src/components/GameCard/GameCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./GameCard.css";
+
+const GameCard = (props) => (
+  <div className="card">
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+     {/* <div className="content">
+      <ul>
+        <li>
+          <strong>Name:</strong> {props.name}
+        </li>
+        <li>
+          <strong>Occupation:</strong> {props.occupation}
+        </li>
+        <li>
+          <strong>Address:</strong> {props.location}
+        </li>
+      </ul>
+    </div> */}
+    <span onClick={() => props.removeCard(props.id)} className="remove">𝘅</span>
+  
+   </div>
+);
+
+export default GameCard;
